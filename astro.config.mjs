447c40169958c0aa4f226f468eaf1ee683153672
Sebastian <sebastian.danielsson@proton.me>
2024-01-03 22:46:35 +0100

    Test less agressive preload

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   output: 'static',
   site: 'https://sebbo.io',
-  prefetch: {
-    prefetchAll: true,
-    defaultStrategy: 'viewport',
-  },
   integrations: [tailwind()],
 });
