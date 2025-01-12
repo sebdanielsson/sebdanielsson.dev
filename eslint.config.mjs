b7e2a7b91d3c707c40b06ca5ddac46b091d71efe
Sebastian <sebastian.danielsson@proton.me>
2025-01-12 17:28:12 +0100

    Fix eslint config, bump deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,12 +5,13 @@ import tseslint from 'typescript-eslint';
 import astro from 'eslint-plugin-astro';
 
 export default [
+  {
+    ignores: ['dist', '.astro'],
+  },
+  {
+    files: ['src/**/*.{js,ts,jsx,tsx,astro}'],
+  },
   eslint.configs.recommended,
   ...tseslint.configs.strict,
   ...astro.configs.recommended,
-  {
-    files: ['src/**/*.{js,ts,jsx,tsx,astro}'],
-    ignores: [],
-    rules: {},
-  },
 ];
