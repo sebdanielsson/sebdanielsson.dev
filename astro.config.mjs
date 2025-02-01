a0a54f0d13dac0e9c983864cc13c1d8518dd4251
Sebastian <sebastian.danielsson@proton.me>
2025-02-01 14:43:18 +0100

    Tailwind v4 (#295)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   site: 'https://sebbo.io',
-  integrations: [tailwind()],
   security: {
     checkOrigin: true,
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
