d2fc1579075134c27b847f031b63f19b06027e8b
Sebastian <sebastian.danielsson@proton.me>
2025-08-03 15:15:43 +0200

    Bump packages, fix stylelint

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -6,6 +6,26 @@ export default {
     'stylelint-config-html/astro',
   ],
   rules: {
+    'import-notation': null,
     'color-hex-length': null,
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'theme',
+          'source',
+          'utility',
+          'variant',
+          'custom-variant',
+          'plugin',
+        ],
+      },
+    ],
+    'function-no-unknown': [
+      true,
+      {
+        ignoreFunctions: ['theme'],
+      },
+    ],
   },
 };
