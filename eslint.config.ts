0683e60d3175245a4f85e6e73ca8ee0ac76722b2
Sebastian <sebastian.danielsson@proton.me>
2025-10-29 20:19:07 +0100

    Chores
    - Remove unused node-version file
    - Use eslint config type
    - Bump deps

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,13 +1,15 @@
+// @ts-check
+
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import astro from "eslint-plugin-astro";
 
-export default tseslint.config(
-  {
-    ignores: ["dist", ".astro"],
-  },
+export default [
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
   astro.configs.recommended,
-);
+  {
+    ignores: ["dist", ".astro"],
+  },
+];
