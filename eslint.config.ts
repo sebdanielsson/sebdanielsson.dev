0a5f4182d4ecbcb86707470c341fe19f61ca27c7
Sebastian <sebastian.danielsson@proton.me>
2025-10-31 13:25:38 +0100

    Bump astro, fix broken eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,9 +6,9 @@ import astro from "eslint-plugin-astro";
 
 export default [
   eslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic,
-  astro.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  ...astro.configs.recommended,
   {
     ignores: ["dist", ".astro"],
   },
